1fd704ff5224532d5f0ac6e26e14146d695bafd5
Lucas Raphael Muniz <114521141+LucasRaphaelMuniz@users.noreply.github.com>
2022-10-04 17:24:56 -0300

    Delete Estrutura-Condicional directory

--- Estrutura-Condicional/Estrutura_Condicional_exerc_glicose.py
+++
@@ -1,8 +0,0 @@
-glicose = float(input("Digite a medida da glicose: "))
-
-if glicose <= 100:
-    print("Classificacao: normal ")
-elif glicose <= 140:
-    print("Classificacao: elevado ")
-else:
-    print("Classificacao: diabetes ")
--- Estrutura-Condicional/Estrutura_Condicional_exerc_tempo_de_jogo.py
+++
@@ -1,9 +0,0 @@
-HoraInicial = int(input("Hora inicial: "))
-HoraFinal = int(input("Hora final: "))
-
-if HoraInicial < HoraFinal:
-    duracao = HoraFinal - HoraInicial
-else:
-    duracao = 24 - HoraInicial + HoraFinal
-
-print(f"O JOGO DUROU {duracao} HORA(S) ")
