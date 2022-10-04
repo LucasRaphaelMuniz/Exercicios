78719f48aa37b726ba881de026d21e28800a9ef1
Lucas Raphael Muniz <114521141+LucasRaphaelMuniz@users.noreply.github.com>
2022-10-04 17:05:10 -0300

    Add files via upload

---
+++ Estruturas-Repetitivas/Estruturas_Repetitivas_exer_validacao_de_nota.py
@@ -0,0 +1,14 @@
+
+nota1 = float(input("Digite a primeira nota: "))
+
+while nota1 <= 0 or nota1 > 10:
+    nota1 = float(input("Nota invalida, digite novamente primeira nota: "))
+
+nota2 = float(input("Digite a segunda nota: "))
+
+while nota2 <= 0 or nota2 > 10:
+    nota2 = float(input("Nota invalida, digite novamente primeira nota: "))
+
+media = (nota1 + nota2) / 2
+
+print(f"MEDIA = {media}")
